0440a9cf744a538696ddf13cc89eac06e6509fcb
Thang Hoang <hoanganhthang357@gmail.com>
2022-12-20 10:52:51 +0700

    Edit code

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
-import { useEffect } from "react";
 import Banner from "../components/banner";
 import Card from "../components/card";
 import { useEffect } from "react";
